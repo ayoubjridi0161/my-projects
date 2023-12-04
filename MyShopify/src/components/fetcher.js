1ae4143940b22db50d99ae0eb42714daab4d1e92
unknown <jridiayoub@isitc.u-sousse.tn>
2023-12-04 20:41:19 +0100

    just lunched the Proload Project

--- MyShopify/src/components/fetcher.js
+++ MyShopify/src/components/fetcher.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'http://localhost:3004/';
 export const fetcher = async (url)=>{
     let responseObject = {errorMessage : '' , data : []}
     try{
